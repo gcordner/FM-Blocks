d54295f3f5c59c1e60bd4e1f1b7892ecd2cb3aef
gcordner <geoffcordner@gmail.com>
2025-09-12 19:32:28 -0700

    init commit

---
+++ build/fm-blocks/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'fa2785f980cb43fdb30f');
---
+++ build/fm-blocks/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'bc2d97e51d4b265d13dd');
