70c011788a7a8d7e82336dc8a0d5adf225016ffe
gcordner <geoffcordner@gmail.com>
2025-10-23 12:05:25 -0700

    fixes to render.php

--- build/front-hero/render.php
+++ build/front-hero/render.php
@@ -1,8 +1,10 @@
 <?php
 $desktop = FM_BLOCKS_ASSETS_URL . 'front-hero/desktop.webp';
-$mobile  = FM_BLOCKS_ASSETS_URL . 'front-hero/mobile.webp';
+// Randomly choose between the two mobile images only
+$mobile_choice = rand(1, 2) === 1 ? 'mobile.webp' : 'mobile02.webp';
+$mobile = FM_BLOCKS_ASSETS_URL . 'front-hero/' . $mobile_choice;
 ?>
-<section class="fm-front-hero" style="--fh-desktop:6/4;--fh-mobile:4/6;">
+<section class="fm-front-hero" style="--fh-desktop:6/4;--fh-mobile:4/5;">
   <picture class="fm-front-hero__media">
     <source media="(max-width: 767px)" srcset="<?php echo esc_url($mobile); ?>" sizes="100vw">
     <source media="(min-width: 768px)" srcset="<?php echo esc_url($desktop); ?>" sizes="100vw">
